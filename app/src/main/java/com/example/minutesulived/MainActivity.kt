9305579c53408c0eacdd074b8673218d33ced57f
dhruv892 <dhruv.170410116002@gmail.com>
2020-07-08 11:56:39 +0530

    finalizing app

---
+++ app/src/main/java/com/example/minutesulived/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.minutesulived
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.time.Year
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnDatePicker.setOnClickListener {view ->
+            clickDatePicker(view)
+        }
+    }
+
+    fun clickDatePicker(view: View){
+
+        val myCalendar= Calendar.getInstance()
+        val year= myCalendar.get(Calendar.YEAR)
+        val month= myCalendar.get(Calendar.MONTH)
+        val day= myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{
+                view, selectedYear, selectedMonth, selectedDayOfMonth ->
+
+            val selectedDate= "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+            tvSelectedDate.text = selectedDate
+
+            val sdf= SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+            val theDate= sdf.parse(selectedDate)
+            val selectedDateInMinutes = theDate!!.time / 60000
+
+            val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+            val currentDateInMinutes = currentDate!!.time / 60000
+
+            val diffInMinutes = currentDateInMinutes - selectedDateInMinutes
+
+            tvSelectedDateInMinutes.text = diffInMinutes.toString()
+
+            Toast.makeText(this, "DatePicker Works", Toast.LENGTH_LONG).show()
+
+
+
+        }
+            ,year, month, day)
+
+        dpd.datePicker.maxDate = Date().time - 86400000
+        dpd.show()
+
+    }
+}
